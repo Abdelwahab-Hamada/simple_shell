20351d085f97304d0a987a5016d836cc211e36d0
root <Muwafaa@github.com>
2023-07-26 13:54:47 -0700

    strtok

---
+++ strtok.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strtok - splits string into tokens with a delimiter
+ * @string: string to be used
+ * @del: delimiter
+ *
+ * Return: tokenized string
+ */
+char *_strtok(char *string, const char *del)
+{
+	char *token;
+	static char *flag;
+
+	if (string != NULL)
+	{
+		flag = string;
+	}
+
+	if (flag == NULL || *flag == '\0')
+	{
+		return (NULL);
+	}
+
+	while (*flag != '\0' && strchr_alt(del, *flag) != NULL)
+	{
+		flag++;
+	}
+
+	if (*flag == '\0')
+	{
+		return (NULL);
+	}
+
+	token = flag;
+	while (*flag != '\0' && strchr(del, *flag) == NULL)
+	{
+		flag++;
+	}
+	if (*flag != '\0')
+	{
+		*flag = '\0';
+		flag++;
+	}
+	return (token);
+}
